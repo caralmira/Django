c399465a6d1c5a9affe8fb97b71d26c35cdab783
carissa.almira <carissa.almira@ui.ac.id>
2022-09-07 20:39:03 +0700

    update app name 2

--- lab-0-carissa/urls.py
+++ lab-0-carissa/urls.py
@@ -1,4 +1,4 @@
-"""Test URL Configuration
+"""lab-0-carissa URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
